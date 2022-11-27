5a5fa5e49da13e62958527452945ed5dafebfdb3
Brian C Sparks <briancsparks@gmail.com>
2022-11-26 21:54:58 -0800

    WIP

---
+++ booya.cpp
@@ -0,0 +1,5 @@
+//
+// Created by brian on 11/26/2022.
+//
+
+#include "booya.h"
---
+++ booya.h
@@ -0,0 +1,14 @@
+//
+// Created by brian on 11/26/2022.
+//
+
+#ifndef KNUCKLEBALL_CPP_BOOYA_H
+#define KNUCKLEBALL_CPP_BOOYA_H
+
+
+class booya {
+
+};
+
+
+#endif //KNUCKLEBALL_CPP_BOOYA_H
